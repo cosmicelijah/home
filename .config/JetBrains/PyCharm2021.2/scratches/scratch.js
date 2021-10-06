3b7fbcb3509a276a3a8b68c14569cd53685092ed
watsonet <watsonet@rose-hulman.edu>
2021-10-05 20:33:40 -0400

    not including a bunch of unnecessary config files

--- .config/JetBrains/PyCharm2021.2/scratches/scratch.js
+++
@@ -1,5 +0,0 @@
-nuts = 2
-for (i = 0, i = 10, i++;) {
-    nuts = nuts - 1 / (2 ** i);
-    console.log(nuts)
-}
--- .config/JetBrains/PyCharm2021.2/scratches/scratch_1.py
+++
@@ -1,9 +0,0 @@
-def foo1(seq):
-    total = 0
-    print(len(seq) // 2)
-    for k in range(len(seq) // 2):
-        total = total + seq[1 + (2 * k)]
-        print([1 + (2 * k)])
-    return total
-
-print(foo1([3, 6]))
